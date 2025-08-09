2b6a96fa4537e0984200ab4730e2f6b4fa4954d0
ComandanteTK <descontraindolive@gmail.com>
2025-08-09 14:44:50 -0300

    Completed HeroSection

---
+++ src/page/home/home.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar";
+import HeroSection from "./HeroSection";
+
+const Home = () => {
+  return (
+    <body>
+      <NavBar />
+      <HeroSection />
+    </body>
+  );
+};
+
+export default Home;
